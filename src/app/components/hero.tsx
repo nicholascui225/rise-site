b6019932dfc88b06a728466e1ddbc3be3fc67ec5
Ramy Kaddouri <106203063+rk234@users.noreply.github.com>
2024-03-14 20:04:42 -0400

    update discord invite

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -17,10 +17,10 @@ export default function Hero() {
 
     function handleResize() {
         //console.log("resize!")
-        if(canvasRef.current) {
+        if (canvasRef.current) {
             const parent = canvasRef.current.parentElement;
 
-            if(parent) {
+            if (parent) {
                 const rect = parent.getBoundingClientRect()
                 canvasRef.current.width = rect.width;
                 canvasRef.current.height = rect.height;
@@ -31,24 +31,24 @@ export default function Hero() {
 
     function draw() {
         const canvas = canvasRef.current;
-        if(canvas) {
+        if (canvas) {
             const context = canvas.getContext("2d");
             context?.clearRect(0, 0, canvas.width, canvas.height)
             context!.strokeStyle = "#fbbf24"
-            
-            context!.translate(canvas.width+20, canvas.height/2);
+
+            context!.translate(canvas.width + 20, canvas.height / 2);
             let size: number = 20
-            for(var i = 0; i < 200; i++) {
-                size*=(Math.sqrt(2.0)/1.3);
-                context!.strokeRect(-size/2, -size/2, size, size)
-                context!.rotate(Math.PI/32);
+            for (var i = 0; i < 200; i++) {
+                size *= (Math.sqrt(2.0) / 1.3);
+                context!.strokeRect(-size / 2, -size / 2, size, size)
+                context!.rotate(Math.PI / 32);
                 context!.lineWidth = 2;
 
             }
         }
     }
 
-    return <main className="bg-gray-900 relative border-y-2 border-y-yellow-600 min-h-96 overflow-hidden" style={{height: "66vh"}}>
+    return <main className="bg-gray-900 relative border-y-2 border-y-yellow-600 min-h-96 overflow-hidden" style={{ height: "66vh" }}>
         <canvas onResize={resizeCall} ref={canvasRef} className="relative top-0 left-0"> </canvas>
         <div className="flex flex-row items-center z-20 absolute top-0 left-0 w-full h-full p-8 bg-opacity-50 bg-slate-950 md:bg-opacity-100 md:bg-transparent md:bg-gradient-to-l md:via-90% md:via-slate-900 md:from-transparent md:to-slate-900">
             <div className="md:w-1/2 w-full">
@@ -58,8 +58,8 @@ export default function Hero() {
                 <p className={`${jbm.className} text-slate-400 text-xs`}>Compete, Learn, and Win Prizes!</p>
                 <div className="flex flex-row mt-4 gap-2">
                     {/* <button disabled={true} className={`${jbm.className} btn-primary`}>Register (Coming Soon...)</button> */}
-                    <a className={`${jbm.className} btn-primary max-w-60 w-full`}  href="https://forms.gle/ctJPAKktFs2bPQYz5" target="_blank" rel="noopener noreferrer">Register</a>
-                    <a className={`${jbm.className} btn-secondary bg-blurple hover:bg-blurple-accent border-blurple hover:border-blurple-accent`} href="https://discord.gg/F2cHnv4vXd" target="_blank" rel="noopener noreferrer">Discord</a>
+                    <a className={`${jbm.className} btn-primary max-w-60 w-full`} href="https://forms.gle/ctJPAKktFs2bPQYz5" target="_blank" rel="noopener noreferrer">Register</a>
+                    <a className={`${jbm.className} btn-secondary bg-blurple hover:bg-blurple-accent border-blurple hover:border-blurple-accent`} href="https://discord.gg/zBQa5MV9Vv" target="_blank" rel="noopener noreferrer">Discord</a>
                 </div>
             </div>
         </div>
