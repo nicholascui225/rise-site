a498eecf11932921f558b02afe38074bfbccfdbb
nicholascui225 <nicholas.cui225@gmail.com>
2025-03-20 20:12:06 -0400

    added metatags

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,40 @@
-import About from './components/about'
-import Hero from './components/hero'
-import Prizes from './components/prizes'
-import Rounds from './components/rounds'
-import Team from './components/team'
-import Schedule from './components/schedule'
-import Faq from './components/faq'
-import Sponsors from './components/sponsors'
+import Head from 'next/head';
+import About from './components/about';
+import Hero from './components/hero';
+import Prizes from './components/prizes';
+import Rounds from './components/rounds';
+import Team from './components/team';
+import Schedule from './components/schedule';
+import Faq from './components/faq';
+import Sponsors from './components/sponsors';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-black">
-      <Hero></Hero>
-      <About></About>
-      <Faq></Faq>
-      {/* <Prizes></Prizes> */}
-      <Schedule></Schedule>
-      <Team></Team>
-      <Sponsors></Sponsors>
-    </main>
-  )
+    <>
+      {/* Add Primary Meta Tags Here */}
+      <Head>
+        <title>CHS RISE Conference</title>
+        <meta
+          name="description"
+          content="Join Centennial High School's RISE conference, a premier event for STEM learning and development. Explore exciting events, workshops, and opportunities."
+        />
+        <meta
+          name="keywords"
+          content="CHS RISE, conference, RISE, Centennial, Centennial High, Centennial High School"
+        />
+        <meta name="author" content="CHS RISE Team" />
+      </Head>
+
+      {/* Your Page Content */}
+      <main className="flex min-h-screen flex-col bg-black">
+        <Hero></Hero>
+        <About></About>
+        <Faq></Faq>
+        {/* <Prizes></Prizes> */}
+        <Schedule></Schedule>
+        <Team></Team>
+        <Sponsors></Sponsors>
+      </main>
+    </>
+  );
 }
